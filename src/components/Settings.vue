93cbdda061421ba2b9265648c0fbf3024755cd43
gyy <407042815@qq.com>
2024-08-19 09:04:17 +0000

    update src/components/Settings.vue.
    Signed-off-by: gyy <407042815@qq.com>

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -6,6 +6,7 @@ import { cdn } from '@/utils/dependency'
   <el-form>
     <el-form-item label="CDN">
       <el-select v-model="cdn" w-full>
+        <el-option value="gcore-jsdelivr" label="Gcore Jsdelivr" />
         <el-option value="jsdelivr" label="jsDelivr" />
         <el-option value="jsdelivr-fastly" label="jsDelivr Fastly" />
         <el-option value="unpkg" label="unpkg" />
